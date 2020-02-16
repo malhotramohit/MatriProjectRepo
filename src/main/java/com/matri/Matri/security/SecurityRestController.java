f94531bdeec165a9cdd7a276fc4f91ca763e6a6e
Vinsh <Vinsh@LAPTOP-6P7DGSNP>
2020-02-16 15:31:54 +0530

    Check for Matrimonial Project

---
+++ src/main/java/com/matri/Matri/security/SecurityRestController.java
@@ -0,0 +1,15 @@
+package com.matri.Matri.security;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityRestController {
+	
+	@GetMapping("/CheckRest")
+	public String checkRest()
+	{
+		return "CheckingRestController";
+	}
+	
+}
