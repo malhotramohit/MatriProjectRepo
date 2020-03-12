7db3ca976ce03d0ade13d7550b609897707a8704
Malhotra <momalhotra@nsf.org>
2020-03-12 14:35:39 +0530

    done

--- src/main/java/com/matri/Matri/security/extraconfig/SecSecurityConfig.java
+++ src/main/java/com/matri/Matri/security/extraconfig/SecSecurityConfig.java
@@ -83,7 +83,7 @@ public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/login*","/login*", "/logout*", "/signin/**", "/signup/**", "/customLogin",
                         "/user/registration*", "/registrationConfirm*", "/expiredAccount*", "/registration*",
                         "/badUser*", "/user/resendRegistrationToken*" ,"/forgetPassword*", "/user/resetPassword*",
-                        "/user/changePassword*", "/emailError*", "/resources/**" , "/css/**","/js/**" ,"/fonts/**", "/images/**" ,"/old/user/registration*","/successRegister*","/qrcode*").permitAll()
+                        "/user/changePassword*", "/emailError*", "/resources/**" , "/css/**","/js/**" ,"/fonts/**", "/images/**" ,"/old/user/registration*","/successRegister*","/qrcode*","/v1/api/dd/religion**").permitAll()
                 .antMatchers("/invalidSession*").anonymous()
                 .antMatchers("/user/updatePassword*","/user/savePassword*","/updatePassword*").hasAuthority("CHANGE_PASSWORD_PRIVILEGE")
                 .anyRequest().hasAuthority("READ_PRIVILEGE")
